11b800a985016c0d6f2826099c470b85ea7d8f8b
Bohny <160973307+Bohny@users.noreply.github.com>
2024-05-02 13:06:06 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function checkForSpam(message) {
+    const normalizedMassage = message.toLowerCase();
+    return normalizedMassage.includes("sale") || normalizedMassage.includes("spam");
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+console.log("Get best sale offers now!".includes("sale"))
